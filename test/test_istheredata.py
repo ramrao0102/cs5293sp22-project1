c59176f3c0d8915dc908723a49e7a00a84913ae5
Ramkishore Rao <ramrao0102@gmail.com>
2022-03-28 14:28:54 +0000

    changes

--- test/test_istheredata.py
+++ test/test_istheredata.py
@@ -6,11 +6,11 @@ import os
 
 
 def test_istheredata():
-    path = "/enroncorpus/write1"
-    obj = os.scandir()
+    path = "enroncorpus/write1"
+    obj = os.scandir(path)
     i = 0
     for entry in obj:
         if entry.is_dir() or entry.is_file():
             i = i+1
-    
+    print(i)
     assert i >0
