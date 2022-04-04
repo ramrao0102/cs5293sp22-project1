56f5a6fce74a47b9deb154bfd501c531b1a8f844
Ramkishore Rao <ramrao0102@gmail.com>
2022-04-04 19:39:20 +0000

    changes

---
+++ test/test_phonefunction.py
@@ -0,0 +1,41 @@
+import pytest
+import project1
+import numpy
+import glob
+import re
+import nltk
+import spacy
+from spacy.matcher import Matcher
+from spacy.matcher import PhraseMatcher
+
+def test_phonefunction():
+    
+    path = "enroncorpus/"+ "*.txt"
+
+    files_grabbed = (glob.glob(path))
+
+    count = 0
+
+    for i in files_grabbed:
+
+        with open(i) as f:
+            writefile = i.replace('enroncorpus', '')
+            data = f.read()
+
+        data2 = data
+
+        data2, count4 = project1.redactphone(data2)
+
+        count += count4
+
+    f = open("stats.txt", "r")
+    Lines = f.readlines()
+
+    a =[]
+
+    for line in Lines:
+        a.append(int(line.strip()))
+
+    assert count == a[2]
+
+    return 0
